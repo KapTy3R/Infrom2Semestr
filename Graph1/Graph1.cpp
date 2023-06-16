e8326c571ca50a07414f8facded6d15f34fe92d1
nikit <nikit@DESKTOP-3FHL12N>
2023-06-16 22:47:48 +0300

    Сделан Graph2

---
+++ Graph1/Graph1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void removeVertex(vector<vector<int>>& graph, int vertex) {
+	int n = graph.size();
+
+	// Удаляем ребра, исходящие из вершины
+	graph[vertex].clear();
+
+	// Удаляем ребра, ведущие к вершине
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < graph[i].size(); j++) {
+			if (graph[i][j] == vertex) {
+				graph[i].erase(graph[i].begin() + j);
+				break;
+			}
+		}
+	}
+}
+
+int main() {
+	int vertexToRemove;
+	vector<vector<int>> graph = {
+		{1, 2},     // Вершина 0 соединена с вершинами 1 и 2
+		{0, 2, 3},  // Вершина 1 соединена с вершинами 0, 2 и 3
+		{0, 1, 3},  // Вершина 2 соединена с вершинами 0, 1 и 3
+		{1, 2}      // Вершина 3 соединена с вершинами 1 и 2
+	};
+	cout << "Default graph:\n";
+	for (int i = 0; i < graph.size(); i++) {
+		cout << i << ": ";
+		for (int j = 0; j < graph[i].size(); j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "\nEnter vertex to remove: ";
+	cin >> vertexToRemove;
+
+	removeVertex(graph, vertexToRemove);
+
+	cout << "New graph:" << endl;
+	for (int i = 0; i < graph.size(); i++) {
+		cout << i << ": ";
+		for (int j = 0; j < graph[i].size(); j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
---
+++ Graph2/Graph2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	int vertexToCheck;
+	// Предопределенный граф в виде списка смежности
+	unordered_map<int, vector<int>> graph = {
+		{0, {1, 3}},
+		{1, {2}},
+		{2, {0, 4}},
+		{3, {2}},
+		{4, {1, 3}}
+	};
+
+	// Вывод исходного графа
+	cout << "Default graph:\n";
+	for (auto& entry : graph) {
+		int vertex = entry.first;
+		vector<int>& adjacentVertices = entry.second;
+
+		cout << "Vertex " << vertex << ": ";
+		for (int i = 0; i < adjacentVertices.size(); i++) {
+			cout << adjacentVertices[i] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "\nEnter vertex: ";
+	cin >> vertexToCheck; // Вершина, для которой нужно найти несмежные вершины
+
+	// Проверка вершин, не смежных с заданной вершиной
+	cout << "\nVertices not adjacent to " << vertexToCheck << " vertex:\n";
+	unordered_set<int> adjacentVertices = unordered_set<int>(graph[vertexToCheck].begin(), graph[vertexToCheck].end());
+	for (auto& entry : graph) {
+		int vertex = entry.first;
+		if (vertex != vertexToCheck && adjacentVertices.find(vertex) == adjacentVertices.end()) {
+			cout << vertex << " ";
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
